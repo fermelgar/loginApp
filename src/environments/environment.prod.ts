9f1058ca853bb525a1b52cfadfff2e2c4e81bb95
Fer Melgar <fernandodmelgar@gmail.com>
2020-09-16 02:21:21 -0600

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB_hifCjvJb7jf3hnZozfzlnJQ6N0iYuXI",
+    authDomain: "ionic5-firebase-authentication.firebaseapp.com",
+    databaseURL: "https://loginapp-d9058.firebaseio.com/",
+    projectId: "login_ionic5-firebase",
+  }
+};
